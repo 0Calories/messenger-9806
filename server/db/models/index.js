00c5d2fc47364df0fb9d92c6885247d03c94769f
Ash Anand <cppanand@gmail.com>
2021-10-22 18:38:18 -0400

    Create new many-to-many relation between messages and users to indicate read status

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -9,6 +9,8 @@ User.belongsToMany(Conversation, { through: User_Conversation });
 Conversation.belongsToMany(User, { through: User_Conversation });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+Message.belongsToMany(User, { through: "message_readers" });
+User.belongsToMany(Message, { through: "message_readers" });
 
 module.exports = {
   User,
